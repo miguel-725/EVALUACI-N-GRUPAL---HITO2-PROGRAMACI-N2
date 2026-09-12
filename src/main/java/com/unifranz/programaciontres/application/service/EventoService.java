2bba0bacaa32c6aacd17625051708e5147b6978a
lpzepablomiguelhurtadoca-cloud <pablomiguelhurtadocastillo@gmail.com>
2026-09-12 11:04:14 -0400

    Inicializar rama

--- src/main/java/com/unifranz/programaciontres/application/service/EventoService.java
+++
@@ -1,7 +0,0 @@
-package com.unifranz.programaciontres.application.service;
-
-import java.util.Map;
-
-public interface EventoService {
-    Map<String, Object> validarAcceso (int edad, boolean pago);
-}
